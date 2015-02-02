f277460b7272b1f5487ca0ec0b21b1b70ed1ab7f
stett <stettjawa@gmail.com>
2015-02-02 11:50:34 -0800

    removed all the extra stuff

--- reporting/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from loans.views import *
-from reporting.views import *
-
-urlpatterns = patterns('reporting.views',
-    #url(r'^ta-year-detail-report/$', 'ta_year_detail_report', name='ta_year_detail_report'),
-    #url(r'^ta-year-detail-report/(?P<budget_year>\d{4})/$', 'ta_year_detail_report', name='ta_year_detail_report'),
-    url(r'^reports/export-csv/(?P<budget_year>\d{4})/$', 'export_year_detail_report_csv', name='export_year_detail_report_csv'),
-)
